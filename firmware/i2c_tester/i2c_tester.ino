29ac66acab608a6294a0600c3df22a32c0603ec2
suzukiyouichi <yo1sjp@outlook.jp>
2022-08-14 08:38:41 +0900

    update opt

--- firmware/i2c_tester/i2c_tester.ino
+++ firmware/i2c_tester/i2c_tester.ino
@@ -21,11 +21,14 @@ int wire_err;
 // 初期化
 void setup() {
 
+  Serial.begin(115200);
+
     // 入力ピン初期化
     pinMode(21, INPUT_PULLUP);
     pinMode(19, INPUT_PULLUP);
     pinMode(22, INPUT_PULLUP);
     pinMode(25, INPUT_PULLUP);
+    pinMode(32, INPUT_PULLUP);
 
     // ステータスLED初期化
     status_rgb =  new Adafruit_NeoPixel(1, 27, NEO_GRB + NEO_KHZ400);
@@ -38,7 +41,7 @@ void setup() {
         Wire.setClock(400000);
     }
 
-    status_rgb->setPixelColor(0, status_rgb->Color(10, 10, 0));
+    status_rgb->setPixelColor(0, status_rgb->Color(10, 0, 0));
     status_rgb->show();
     last_status = 0;
 
@@ -47,7 +50,7 @@ void setup() {
 
 // キーの入力状態を取得
 void key_read() {
-    int i;
+    int i, r, s, e;
 
     // 入力データクリア
     for (i=0; i<16; i++) {
@@ -57,37 +60,41 @@ void key_read() {
 
     // ダイレクトキー入力取得
 
-    if (!digitalRead(21)) input_key[1] = 1;
-    if (!digitalRead(19)) input_key[2] = 1;
+    if (!digitalRead(21)) input_key[0] = 1;
+    if (!digitalRead(19)) input_key[1] = 1;
     if (!digitalRead(22)) input_key[2] = 1;
     if (!digitalRead(25)) input_key[3] = 1;
+    if (!digitalRead(32)) input_key[4] = 1;
 
-    Wire.beginTransmission(0x40);
-    Wire.write(0x10);
-    wire_err = Wire.endTransmission();
-    Wire.requestFrom(0x40, 1);
-    // while (Wire.available()) 
-    input_key[0] = Wire.read(); // データ全部受け取る
+    s = micros();
+    Wire.requestFrom(0x44, 1);
+    r = Wire.read(); // データ受け取る
+    e = micros();
+    if (r > 0) Serial.printf("R: %d %d\n", r, (e - s));
+    if (r & 0x01) input_key[5] = 1;
+    if (r & 0x02) input_key[6] = 1;
+    if (r & 0x04) input_key[7] = 1;
+    if (r & 0x08) input_key[8] = 1;
 
 }
 
 // 動作ループ
 void loop() {
-    int i;
+    int i, n;
     bool change_flag = false;
 
     // キーの入力情報取得
     key_read();
 
-    // 前回の入力から変更があったかチェック
-    for (i=0; i<4; i++) {
+    // 今回入力があるかチェック
+    for (i=0; i<9; i++) {
         if (input_key[i]) change_flag = true;
     }
 
-    // 変更が無ければ何もしない
+    // 入力無し
     if (!change_flag) {
         if (last_status > 0) {
-            status_rgb->setPixelColor(0, status_rgb->Color(10, 10, 0));
+            status_rgb->setPixelColor(0, status_rgb->Color(10, 0, 0));
             status_rgb->show();
             /*
             Wire.beginTransmission(0x40);
@@ -100,15 +107,21 @@ void loop() {
         return;
     }
 
-    if (last_status != input_key[0]) {
-        status_rgb->setPixelColor(0, status_rgb->Color(0, (input_key[0] & 0x01) * 10, (input_key[0] & 0x02) * 5));
+    n = input_key[0] + input_key[1] + input_key[2] + input_key[3] + input_key[4] + input_key[5] + input_key[6] + input_key[7] + input_key[8];
+
+    if (last_status != n) {
+        if (input_key[7] || input_key[8]) {
+            status_rgb->setPixelColor(0, status_rgb->Color(0, input_key[7] * 10, input_key[8] * 10));
+        } else {
+            status_rgb->setPixelColor(0, status_rgb->Color(0, 10, 10));
+        }
         status_rgb->show();
         /*
         Wire.beginTransmission(0x40);
         Wire.write(0x01);
         wire_err = Wire.endTransmission();
         */
-        last_status = input_key[0];
+        last_status = n;
     }
 
     delay(50);
