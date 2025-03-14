249135f612baa43c65da7c3f05bfd6eef37a601e
yo1sjp <yo1sjp@outlook.jp>
2025-03-14 11:16:58 +0900

    add azcard

--- firmware/az_keyboard/az_config.h
+++ firmware/az_keyboard/az_config.h
@@ -2,10 +2,10 @@
 #define AzConfig_h
 
 // キーボードの接続ライブラリ ( 0=NimBLE / 1=NomalBLE / 2=USB )
-#define KEYBOARD_TYPE 1
+#define KEYBOARD_TYPE 2
 
 // ESP32 のタイプ ( 0 = ノーマル ESP32 / 8 = ESP32C6 / 9 = ESP32S3 )
-#define CPUTYPE_ESP32 8
+#define CPUTYPE_ESP32 9
 
 // WIFI機能（ 0 = WIFI機能なし / 1 = WIFI機能あり ）
 #define WIFI_FLAG 0
@@ -17,6 +17,7 @@
 //   3 = AZ-Magicpad
 //   4 = AZSENSOR(XIAO ESP32S3)
 //   5 = XIAO ESP32C6
-#define SETTING_JSON_DEFAULT_TYPE 5
+//   6 = AZCARD(XIAO ESP32S3)
+#define SETTING_JSON_DEFAULT_TYPE 6
 
 #endif // AzConfig_h
